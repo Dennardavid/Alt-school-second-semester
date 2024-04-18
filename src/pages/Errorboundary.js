91b4eb1f8146538aac3f60341eadab95ac36207f
Dennar David <dennardavid@gmail.com>
2024-04-18 20:35:44 +0100

    Error boundary

--- src/pages/Errorboundary.js
+++ src/pages/Errorboundary.js
@@ -1,10 +1,11 @@
 import Errorpic from "../assets/pngwing.com.png";
+import "./Errorpage.css";
 
 function ErrorFetchingPage() {
   return (
-    <div>
-      <img src={Errorpic} alt="Error" />
-      <h1>Error!!</h1>
+    <div className="fullpage">
+      <img src={Errorpic} alt="Error" className="errorimg " />
+      <h1 className="errorheader">Error!!</h1>
       <p>There was an error fetching page...</p>
     </div>
   );
